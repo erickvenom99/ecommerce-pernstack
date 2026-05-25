bcee80ba198904565c2926a57291378d5a491644
erickvenom99 <prevenakhtar@mail.com>
2026-05-25 13:07:01 +0100

    feat: add Inngest serve route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDelete, syncUserUpdate } from "../../../inngest/functions";
+import { deleteCouponOnExpiry, syncUserCreation, syncUserDelete, syncUserUpdate } from "../../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdate,
-    syncUserDelete
+    syncUserDelete,
+    deleteCouponOnExpiry
   ],
   signingKey: process.env.INNGEST_SIGNING_KEY,
   
