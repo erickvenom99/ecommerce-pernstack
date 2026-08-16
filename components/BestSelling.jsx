9dca27b4ebc661f297abe87c4fe5a070a601b976
erickvenom99 <prevenakhtar@mail.com>
2026-08-16 14:50:22 +0100

    feat updated the latestproduct.jsx and bestseller.jsx to show 4 products in a row

--- components/BestSelling.jsx
+++ components/BestSelling.jsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react'
 import Title from './Title'
 import ProductCard from './ProductCard'
 import { useSelector } from 'react-redux'
@@ -9,12 +10,21 @@ const BestSelling = () => {
     const products = useSelector(state => state.product.list)
 
     return (
-        <div className='px-6 my-30 max-w-6xl mx-auto '>
-            <Title  title='Best Selling' description={`Showing ${products.length < displayQuantity ? products.length : displayQuantity} of ${products.length} products`} href='/shop' />
-            <div className='mt-12  grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12'>
-                {products.slice().sort((a, b) => b.rating.length - a.rating.length).slice(0, displayQuantity).map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
+        <div className='px-6 my-30 max-w-6xl mx-auto'>
+            <Title 
+                title='Best Selling' 
+                description={`Showing ${products.length < displayQuantity ? products.length : displayQuantity} of ${products.length} products`} 
+                href='/shop' 
+            />
+            {/* Same grid layout and properties as LatestProducts.jsx for exact visual consistency */}
+            <div className='mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                {products
+                    .slice()
+                    .sort((a, b) => (b.rating?.length || 0) - (a.rating?.length || 0))
+                    .slice(0, displayQuantity)
+                    .map((product, index) => (
+                        <ProductCard key={product._id || index} product={product} />
+                    ))}
             </div>
         </div>
     )
